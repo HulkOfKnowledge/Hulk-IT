55710efdfb979657cf2ff770a7a0c7a00f72fbdd
HulkOfKnowledge <ifebuemedaniel7@gmail.com>
2024-01-16 18:25:33 +0100

    Feature Complete: Discover Component

--- frontend/components/NavBar/Discover/Discover.jsx
+++ frontend/components/NavBar/Discover/Discover.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import Link from "next/link";
 import Style from "./Discover.module.css";
 
 const Discover = () => {
+
+    // Discover Nav Menu
+    const discover = [
+        {
+            name: "Collection",
+            link: "collection",
+        },
+        {
+            name: "Search",
+            link: "search",
+        },
+        {
+            name: "Author Profile",
+            link: "author-profile",
+        },
+        {
+            name: "NFT Details",
+            link: "/nft-details",
+        },
+        {
+            name: "Account Settings",
+            link: "account-settings",
+        },
+        {
+            name: "Connect Wallet",
+            link: "connect-wallet",
+        },
+        {
+            name: "Blog",
+            link: "blog",
+        }
+    ];
     return(
-        <div>Discover</div>
+        <div>
+            {discover.map(
+                (el,i)=>(
+                    <div key={i+1} className={Style.discover}>
+                        <Link href={{pathname: `${el.link}`}}>{el.name}</Link>
+                    </div>
+                )
+            )}
+        </div>
     )
 }
 
