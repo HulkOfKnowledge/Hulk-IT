a895ebddcc9f35ebddbcb6c9086be7c205ceb7e8
HulkOfKnowledge <ifebuemedaniel7@gmail.com>
2024-01-17 01:20:36 +0100

    Feature Complete: HeroSection Component and Styling

---
+++ frontend/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,49 @@
+import React,{useEffect,useRef } from "react";
+import lottie from 'lottie-web';
+import animationData from '../../img/hero.json';
+
+
+//INTERNAL IMPORT
+import Style from "./HeroSection.module.css";
+import { Button } from "../componentsindex";
+import images from "../../img";
+
+const LottieAnimation = ({ animationData, loop = true }) => {
+    const containerRef = useRef(null);
+  
+    useEffect(() => {
+      const anim = lottie.loadAnimation({
+        container: containerRef.current,
+        renderer: 'svg',
+        loop,
+        autoplay: true,
+        animationData,
+      });
+  
+      return () => anim.destroy(); // Cleanup animation on unmount
+    }, [animationData, loop]);
+  
+    return <div ref={containerRef}></div>;
+  };
+
+const HeroSection = () => {
+  return (
+    <div className={Style.heroSection}>
+      <div className={Style.heroSection_box}>
+        <div className={Style.heroSection_box_left}>
+          <h1>Discover, collect, and sell NFTs 🖼️</h1>
+          <p>
+            Discover the most outstanding NTFs in all topics of life. Creative
+            your NTFs and sell them
+          </p>
+          <Button btnName="Start your search" />
+        </div>
+        <div className={Style.heroSection_box_right}>
+          <LottieAnimation animationData={animationData} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
