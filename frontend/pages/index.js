429563268d5f406c162d4f74ca06dc0778ab3e20
HulkOfKnowledge <ifebuemedaniel7@gmail.com>
2024-01-17 11:58:25 +0100

    Feature Complete: NFTCard Component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection,Service,NFTSlider,Subscribe,Title,Category,Filter } from "../components/componentsindex";
+import { HeroSection,Service,NFTSlider,Subscribe,Title,Category,Filter,NFTCard } from "../components/componentsindex";
 
 const Home = () => {
   return (
@@ -11,6 +11,7 @@ const Home = () => {
       <Service/>
       <NFTSlider/>
       <Filter/>
+      <NFTCard/>
       <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories."/>
       <Category/>
       <Subscribe/>
