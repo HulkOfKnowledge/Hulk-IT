9182aeb498291e0612e611323deb0ca9a50f32a8
HulkOfKnowledge <ifebuemedaniel7@gmail.com>
2024-01-18 02:58:36 +0100

    Feature Complete: Collection Component
    ps: css-ish

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,19 +2,27 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection,Service,NFTSlider,Subscribe,Title,Category,Filter,NFTCard } from "../components/componentsindex";
+import { HeroSection,Service,NFTSlider,Subscribe,Title,Category,Filter,NFTCard,Collection } from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
-      <Service/>
-      <NFTSlider/>
-      <Filter/>
-      <NFTCard/>
-      <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories."/>
-      <Category/>
-      <Subscribe/>
+      <Service />
+      <NFTSlider />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
     </div>
   );
 };
